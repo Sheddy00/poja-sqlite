9b4318861111c107ca6407d44a6b0eff7e34b073
Sheddy00 <hei.sheddy.2@gmail.com>
2023-12-22 10:29:34 +0400

    fix

--- src/main/java/com/poja/sqlite/endpoint/rest/controller/health/BigSumController.java
+++ src/main/java/com/poja/sqlite/endpoint/rest/controller/health/BigSumController.java
@@ -1,2 +1,26 @@
-package com.poja.sqlite.endpoint.rest.controller.health;public class BigSumController {
+package com.poja.sqlite.endpoint.rest.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+
+@RestController
+public class BigSumController {
+
+    @GetMapping("/big-sum")
+    public String calculateBigSum(@RequestParam("a") String a,@RequestParam("b") String b) {
+        try {
+            BigInteger numA = new BigInteger(a);
+            BigInteger numB = new BigInteger(b);
+
+            BigInteger sum = numA.add(numB);
+
+            return sum.toString();
+        } catch (NumberFormatException e) {
+            return "Parameters a and b must be valid numbers.";
+        }
+    }
 }
